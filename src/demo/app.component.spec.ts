b3578987a5eb4437b4575f3cddce28a81b9928d5
Michael Spaxman <seanforyou23@gmail.com>
2018-05-29 15:20:57 -0400

    [karma-config] add basic karma config, add a simple test to run against

---
+++ src/demo/app.component.spec.ts
@@ -0,0 +1,44 @@
+import {
+  async,
+  fakeAsync,
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+
+describe('App Component - ', () => {
+
+  let appComponent: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [],
+      providers: []
+    })
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      appComponent = fixture.componentInstance;
+    });
+
+  }));
+
+  afterEach(() => {
+    appComponent = null;
+    fixture = null;
+  });
+
+  it('should have a valid fixture', () => {
+    expect(fixture).toBeTruthy();
+  });
+
+  it('should set a custom tooltip placement', () => {
+    expect(1).toBeTruthy();
+  });
+});
